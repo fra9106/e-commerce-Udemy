7fa9b4e5d792f29eca4c6806e0b1aa83b101519a
fra9106 <contact@monpersoweb.fr>
2021-06-07 17:41:08 +0200

    conditions generales register form

---
+++ src/Controller/CGUController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CGUController extends AbstractController
+{
+    /**
+     * @Route("/conditions-generales", name="app_conditions_generales")
+     */
+    public function conditionsGenerales(): Response
+    {
+        return $this->render('cgu/conditions_generales.html.twig');
+    }
+}
